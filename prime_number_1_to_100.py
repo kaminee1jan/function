c55b3dbb500fc53f1dc631a26fc4ace0944f65a2
kaminee1jan <83804073+kaminee1jan@users.noreply.github.com>
2021-07-07 11:09:19 +0530

    Add files via upload

---
+++ prime_number_1_to_100.py
@@ -0,0 +1,33 @@
+def prime_number():
+    num = 2
+    while num <=100:
+        count = 0
+        i = 2
+        while i < num:
+            if num % i == 0:
+                count = count + 1
+                break
+            i = i + 1
+        if count == 0:
+            print(num)
+        num = num + 1
+        a=num
+prime_number()
+
+# num = 2
+# while(num <=100):
+#     count = 0
+#     i = 2
+#     while(i < num):
+#         if(num % i == 0):
+#             count = count + 1
+#             break
+#         i = i + 1
+#     if (count == 0):
+#         print(num)
+#     num = num + 1
+#     a=num
+
+
+
+
